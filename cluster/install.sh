883410c042978ac8092fda24b8f32c1949e71431
Gijs van Dulmen <gijs@vandulmen.net>
2021-09-21 14:44:13 +0200

    Upgrade to istio 1.11.2

--- cluster/install.sh
+++ cluster/install.sh
@@ -26,7 +26,7 @@ minikube -p sample-istio-minikube start --memory=8192 --cpus=4 --vm=true --drive
 
 
 ## Download istio if not already downloaded
-export ISTIO_VERSION=1.8.2
+export ISTIO_VERSION=1.11.2
 ./install_istio.sh ${ISTIO_VERSION}
 export PATH=$PWD/istio-${ISTIO_VERSION}/bin:$PATH
 
@@ -44,24 +44,25 @@ kubectl -n istio-system wait --for=condition=available --timeout=600s deployment
 
 ##### PROMETHEUS #####
 echo "Installing prometheus"
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/prometheus.yaml || :
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.11/samples/addons/prometheus.yaml || :
 
 ##### KIALI 1.26 #####
 
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.11/samples/addons/kiali.yaml || :
+
 # do this twice because of some odd error of missing CRD's
 # see: https://github.com/istio/istio/issues/27417
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/kiali.yaml || :
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/kiali.yaml || :
+# kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.11/samples/addons/kiali.yaml || :
 
 # Open Kiali on NodePort
 kubectl -n istio-system patch svc kiali --patch '{"spec": { "type": "NodePort", "ports": [ { "name": "http", "nodePort": 30123, "port": 20001, "protocol": "TCP", "targetPort": 20001 }, { "name": "http-metrics", "nodePort": 30333, "port": 9090, "protocol": "TCP", "targetPort": 9090 } ] } }'
 
 #### JAEGER #####
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/jaeger.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.11/samples/addons/jaeger.yaml
 kubectl -n istio-system patch svc tracing --patch '{"spec": { "type": "NodePort", "ports": [ { "name": "http-query", "nodePort": 30567, "port": 80, "protocol": "TCP", "targetPort": 16686 } ] } }'
 
 ##### GRAFANA #####
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.8/samples/addons/grafana.yaml
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.11/samples/addons/grafana.yaml
 kubectl -n istio-system patch svc grafana --patch '{"spec": { "type": "NodePort", "ports": [ { "name": "service", "nodePort": 30789, "port": 3000, "protocol": "TCP", "targetPort": 3000 } ] } }'
 
 ##### FLAGGER #####
--- traffic-generator/run.sh
+++ traffic-generator/run.sh
@@ -5,12 +5,12 @@ OS="$(uname)"
 if [ "x${OS}" = "xDarwin" ] ; then
   open http://${MINIKUBE_IP}:30567
   open http://${MINIKUBE_IP}:30789
-  open http://${MINIKUBE_IP}:30123/kiali/console/graph/namespaces/\?edges\=requestsPerSecond\&graphType\=versionedApp\&namespaces\=default%2Cistio-system\&unusedNodes\=false\&operationNodes\=false\&injectServiceNodes\=true\&duration\=60\&refresh\=15000\&layout\=dagre
+  open http://${MINIKUBE_IP}:30123/kiali/console/graph/namespaces/?edges=requestsPerSecond%2CresponseTime%2Crt95%2Cthroughput%2CthroughputRequest%2CtrafficDistribution%2CtrafficRate&graphType=versionedApp&namespaces=default%2Cistio-system&unusedNodes=false&operationNodes=true&injectServiceNodes=true&duration=60&refresh=15000&layout=dagre&traffic=grpc%2CgrpcRequest%2Chttp%2ChttpRequest%2Ctcp%2CtcpSent&idleNodes=false&idleEdges=false
 else
   echo "Please open the following websites manually in your browser:"
   echo http://${MINIKUBE_IP}:30567
   echo http://${MINIKUBE_IP}:30789
-  echo http://${MINIKUBE_IP}:30123/kiali/console/graph/namespaces/\?edges\=requestsPerSecond\&graphType\=versionedApp\&namespaces\=default%2Cistio-system\&unusedNodes\=false\&operationNodes\=false\&injectServiceNodes\=true\&duration\=60\&refresh\=15000\&layout\=dagre
+  echo http://${MINIKUBE_IP}:30123/kiali/console/graph/namespaces/?edges=requestsPerSecond%2CresponseTime%2Crt95%2Cthroughput%2CthroughputRequest%2CtrafficDistribution%2CtrafficRate&graphType=versionedApp&namespaces=default%2Cistio-system&unusedNodes=false&operationNodes=true&injectServiceNodes=true&duration=60&refresh=15000&layout=dagre&traffic=grpc%2CgrpcRequest%2Chttp%2ChttpRequest%2Ctcp%2CtcpSent&idleNodes=false&idleEdges=false
   sleep 120
 fi
 node index.js ${MINIKUBE_IP}
